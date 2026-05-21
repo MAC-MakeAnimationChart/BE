68e7675a6167ce1bbec64bcbab1037bf6dcb73f7
suerovr <suerovr20@gmail.com>
2026-05-21 19:04:49 +0900

    lestgo

---
+++ src/main/java/com/mac/projectmac/ProjectmacApplication.java
@@ -0,0 +1,13 @@
+package com.mac.projectmac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectmacApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectmacApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mac/projectmac/ProjectmacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mac.projectmac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectmacApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
