5803aa6dd9a527e8a65ec4a8d572baf0b5a57cdc
lgw-create <lgw1234562@gmail.com>
2026-06-08 23:53:52 +0900

    에러코드 확인

--- src/main/java/com/mac/projectmac/global/api/common/ApiResponse.java
+++ src/main/java/com/mac/projectmac/global/api/common/ApiResponse.java
@@ -10,22 +10,15 @@ public record ApiResponse<T>(
         T data
 ) {
 
-    // GET, PUT, PATCH
     public static <T> ApiResponse<T> success(String code, String message, T data) {
         return new ApiResponse<>(Instant.now(), 200, code, message, data);
     }
 
-    // POST
     public static <T> ApiResponse<T> created(String code, String message, T data) {
         return new ApiResponse<>(Instant.now(), 201, code, message, data);
     }
 
-    // 응답을 안하는
     public static ApiResponse<Void> success(String code, String message) {
-        return new ApiResponse<>(Instant.now(), 201, code, message, null);
+        return new ApiResponse<>(Instant.now(), 200, code, message, null);
     }
-
-    // 비동기 삭제 시 204 추후 사용
-    // 일반 삭제 시 프론트로 반환할 값 없음.
-
 }
