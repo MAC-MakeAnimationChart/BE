b5fd0e966326203b8a19cfd90de3678f05ee7a89
rladydwns2361 <gimyongjun796@gmail.com>
2026-06-26 21:08:48 +0900

    flyway 적용

--- src/main/java/com/mac/projectmac/chart/option/presentation/api/request/UpdateChartOptionRequest.java
+++ src/main/java/com/mac/projectmac/chart/option/presentation/api/request/UpdateChartOptionRequest.java
@@ -2,10 +2,15 @@ package com.mac.projectmac.chart.option.presentation.api.request;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.mac.projectmac.chart.option.application.command.UpdateChartOptionCommand;
+import io.swagger.v3.oas.annotations.media.Schema;
 
+@Schema(description = "차트 옵션 저장/수정 요청")
 public record UpdateChartOptionRequest(
+        @Schema(description = "차트 타입", example = "BAR", requiredMode = Schema.RequiredMode.REQUIRED)
         String chartType,
+        @Schema(description = "차트 필드와 데이터 컬럼 매핑", requiredMode = Schema.RequiredMode.REQUIRED)
         JsonNode dataMapping,
+        @Schema(description = "차트 스타일 옵션")
         JsonNode styleOption
 ) {
 
