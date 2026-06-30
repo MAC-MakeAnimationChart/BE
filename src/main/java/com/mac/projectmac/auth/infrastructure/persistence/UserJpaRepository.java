9ec010fd1b912a1578f66658ebcb8a8138d2e22f
sunkyung <sunkoungz@gmail.com>
2026-06-30 22:27:01 +0900

    fix: method

--- src/main/java/com/mac/projectmac/auth/infrastructure/persistence/UserJpaRepository.java
+++ src/main/java/com/mac/projectmac/auth/infrastructure/persistence/UserJpaRepository.java
@@ -8,5 +8,6 @@ import java.util.Optional;
 public interface UserJpaRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
-    boolean existsByNickname(String nickname);
+    boolean existsByname(String name);
+
 }
